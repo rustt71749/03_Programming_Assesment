3daa7317283831de4549e5af6196347222d66bf8
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-06-17 12:19:31 +1200

    fixn

--- 01_PA_get_input.py
+++ 01_PA_get_input.py
@@ -42,6 +42,5 @@ def int_check(question, low=None, high=None):
 
 # Main routine
 
-lowest = int_check("What range do you want to play with? ")
-highest = int_check("High Number: ", lowest + 1)
+highest = int_check("What range do you want to play with? ", 1)
 rounds = int_check("How many rounds would you like to play? ", 1)
