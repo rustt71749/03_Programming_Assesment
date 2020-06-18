488822761d1b6187efbaf42f0f744116d132c7f5
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-06-19 10:54:00 +1200

    Fixed kinda

--- 04_give feedback.py
+++ 04_give feedback.py
@@ -13,25 +13,25 @@ ANSWER3 = EQUATION_NUMBER1 * EQUATION_NUMBER2
 ANSWER4 = EQUATION_NUMBER1 / EQUATION_NUMBER2
 
 # compares user answer to actual answer
-equation1 = int_check(input("What does {} + {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
+equation1 = int(input("What does {} + {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
 if equation1 != ANSWER1:
     print("Sorry that answer is not correct. The answer is {}".format(ANSWER1))
 else:
     print("That answer is correct. Great job!")
 
-equation2 = int_check(input("What does {} - {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
+equation2 = int(input("What does {} - {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
 if equation2 != ANSWER2:
     print("Sorry that answer is not correct. The answer is {}".format(ANSWER2))
 else:
     print("That answer is correct. Great job!")
 
-equation3 = int_check(input("What does {} x {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
+equation3 = int(input("What does {} x {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
 if equation3 != ANSWER3:
     print("Sorry that answer is not correct. The answer is {}".format(ANSWER3))
 else:
     print("That answer is correct. Great job!")
 
-equation4 = int_check(input("What does {} / {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
+equation4 = int(input("What does {} / {} equal? ".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
 if equation4 != ANSWER4:
     print("Sorry that answer is not correct. The answer is {}".format(ANSWER4))
 else:
