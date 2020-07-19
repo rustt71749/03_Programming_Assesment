cc79268e693fca295ed082766035f4dacc5dc5e2
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-07-20 09:55:03 +1200

    ggn

--- 03_PA_generate_equations.py
+++ 03_PA_generate_equations.py
@@ -10,4 +10,3 @@ equation1 = int(input("What does {} + {} equal?".format(EQUATION_NUMBER1, EQUATI
 equation2 = int(input("What does {} - {} equal?".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
 equation3 = int(input("What does {} x {} equal?".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
 equation4 = int(input("What does {} / {} equal?".format(EQUATION_NUMBER1, EQUATION_NUMBER2)))
-
--- 05_answer_list.py
+++ 05_answer_list.py
@@ -43,6 +43,10 @@ def int_check(question, low=None, high=None):
             print(error)
             continue
 
+# adds questions to incorrect and correct lists to be displayed later
+incorrect_answer = []
+correct_answer = []
+
 # Main routine
 lowest = int_check("What is the lowest number you would like to play with? ")
 highest = int_check("What is the highest number you would like to play with? ", lowest + 1)
@@ -61,24 +65,25 @@ answer4 = equation_number1 // equation_number2
 # Compares user answer to actual answer
 equation1 = int_check(input("What does {} + {} equal? ".format(equation_number1, equation_number2)))
 if equation1 != answer1:
-    print("Sorry, that answer is not correct. The answer is {}".format(answer1))
+    print("Sorry that answer is not correct. The answer is {}".format(answer1))
+
 else:
     print("That answer is correct. Great job!")
 
 equation2 = int_check(input("What does {} - {} equal? ".format(equation_number1, equation_number2)))
 if equation2 != answer2:
-    print("Sorry, that answer is not correct. The answer is {}".format(answer2))
+    print("Sorry that answer is not correct. The answer is {}".format(answer2))
 else:
     print("That answer is correct. Great job!")
 
 equation3 = int_check(input("What does {} x {} equal? ".format(equation_number1, equation_number2)))
 if equation3 != answer3:
-    print("Sorry, that answer is not correct. The answer is {}".format(answer3))
+    print("Sorry that answer is not correct. The answer is {}".format(answer3))
 else:
     print("That answer is correct. Great job!")
 
 equation4 = int_check(input("What does {} / {} equal? ".format(equation_number1, equation_number2)))
 if equation4 != answer4:
-    print("Sorry, that answer is not correct. The answer is {}".format(answer4))
+    print("Sorry that answer is not correct. The answer is {}".format(answer4))
 else:
     print("That answer is correct. Great job!")
