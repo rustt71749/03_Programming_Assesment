31e2604f6e63e171807cbcb0ec343a3eb73e7171
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-07-21 14:44:10 +1200

    working

---
+++ 05_answer_list_trial2.py
@@ -0,0 +1,92 @@
+# Component 4
+# compare user input for answer and actual answer
+# give appropriate feedback based on input
+
+import random
+
+# Number checking function:
+
+
+def int_check(question, low=None, high=None):
+    # error messages
+    if low is not None and high is not None:
+        error = "Please enter an integer between {} and {} " \
+                "(inclusive)".format(low, high)
+    elif low is not None and high is None:
+        error = "Please enter an integer that is more than or " \
+                "equal to {}".format(low)
+    elif low is None and high is not None:
+        error = "Please enter an integer that is less than or " \
+                "equal to {}".format(high)
+
+    else:
+        error = "Please enter an integer"
+
+    while True:
+
+        try:
+            response = int(input(question))
+
+            # Checks response is not low
+            if low is not None and response < low:
+                print(error)
+                continue
+
+            # Checks response is not too high
+            if high is not None and response > high:
+                print(error)
+                continue
+
+            return response
+
+        except ValueError:
+            print(error)
+            continue
+
+# adds questions to incorrect and correct lists to be displayed later
+incorrect_answer = []
+correct_answer = []
+
+# Main routine
+lowest = int_check("What is the lowest number you would like to use? ")
+highest = int_check("What is the highest number you would like to use? ", lowest + 1)
+questions = int_check("How many questions would you like? ", 1, 10)
+
+# randomly generates numbers between user input
+equation_number1 = random.randint(lowest, highest)
+equation_number2 = random.randint(lowest, highest)
+
+# randomly generates
+# Compares user answer to actual answer
+question = "What does {} + {} equal? ".format(equation_number1, equation_number2)
+equation1 = int_check(question, -(highest*highest), highest*highest)
+
+if equation1 != answer1:
+    print("Sorry, that answer is not correct. The answer is {}.".format(answer1))
+else:
+    print("That answer is correct. Great job!")
+
+question2 = "What does {} - {} equal? "
+equation2 = int_check(question2, -(highest*highest), highest*highest)
+
+if equation2 != answer2:
+    print("Sorry, that answer is not correct. The answer is {}.".format(answer2))
+else:
+    print("That answer is correct. Great job!")
+
+
+question3 = "What does {} x {} equal? ".format(equation_number1, equation_number2)
+equation3 = int_check(question3, -(highest*highest), highest*highest)
+
+if equation3 != answer3:
+    print("Sorry, that answer is not correct. The answer is {}.".format(answer3))
+else:
+    print("That answer is correct. Great job!")
+
+question4 = "What does {} / {} equal ? ".format(equation_number1, equation_number2)
+equation4 = int_check(question4, -(highest*highest), highest*highest)
+
+if equation4 != answer4:
+    print("Sorry, that answer is not correct. The answer is {}.".format(answer4))
+else:
+    print("That answer is correct. Great job!")
