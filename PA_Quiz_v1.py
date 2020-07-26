00de6ff83d11dadd2aa077617be98b28ed23eb9f
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-07-27 09:54:00 +1200

    Finished Final quiz code

--- PA_Quiz_v1.py
+++ PA_Quiz_v1.py
@@ -5,7 +5,7 @@
 
 import random
 
-# String Checker by Miss Gottshalk
+# String Checker by Miss Gottschalk
 # Checks user input (either full word or first letter of word)
 
 
@@ -16,14 +16,15 @@ def string_checker(question, to_check):
         response = input(question).lower()
 
         # check response is in list OR that it's the first letter of an item in the list
-        for item in to_check:
-            if response == item:
+        for item2 in to_check:
+            if response == item2:
                 return response
-            elif response == item[0]:
-                return item
+            elif response == item2[0]:
+                return item2
 
         # if item not in list, print error
         print("Sorry, that response in invalid")
+        print("Please enter yes or no")
 
 # Integer checking function
 
@@ -46,6 +47,7 @@ def int_check(question, low=None, high=None):
     while True:
 
         try:
+            # ask user question and expect number input
             response = int(input(question))
 
             # Checks response is not low
@@ -60,6 +62,7 @@ def int_check(question, low=None, high=None):
 
             return response
 
+        # if input is not an integer, print error
         except ValueError:
             print(error)
             continue
@@ -97,83 +100,86 @@ statement_generator("| | | | | Welcome to The Math Quiz | | | | |", "-")
 print()
 instructions()
 
-# gets user input for numbers and questions
-lowest = int_check("What is the lowest number you would like to use? ")
-highest = int_check("What is the highest number you would like to use? ", lowest + 1)
-
-questions = int_check("How many questions would you like? ", 1, 10)
-print()
-
-
-# list for questions and answers to be used at end of game
-right = 0
-wrong = 0
-
-quiz_history = []
-
-# operation list
-operations = ["+", "-", "*"]
-
-# Start quiz
-for item in range(1, questions + 1):
-
-    # randomly generates numbers between user input
-    equation_num1 = random.randint(lowest, highest)
-    equation_num2 = random.randint(lowest, highest)
-
-    # randomly chooses operation
-    operation = random.choice(operations)
-
-    # generates equations and equation answers from randomly generated operations and numbers
-    display_equation = "What does {} {} {} equal? ".format(equation_num1, operation, equation_num2)
-    equation = "{} {} {}".format(equation_num1, operation, equation_num2)
-    correct_answer = eval(equation)
-
+# if user presses enter program loops
+go_again = ""
+while go_again == "":
+    # gets user input for numbers and questions
     print()
-    # generates answer for any equation generated
-    answer = int_check(display_equation, -(highest * highest), (highest * highest))
+    lowest = int_check("What is the lowest number you would like to use? ")
+    highest = int_check("What is the highest number you would like to use? ", lowest + 1)
+    print()
+    questions = int_check("How many questions would you like? ", 1, 10)
+    print()
 
-    # compares user answer to actual answer
-    if answer == correct_answer:
-        feedback = "* Your answer of {} was correct. Great job! *".format(answer)
+    # list for questions and answers to be used at end of game
+    right = 0
+    wrong = 0
+
+    quiz_history = []
+
+    # operation list
+    operations = ["+", "-", "*"]
+
+    # Start quiz
+    for item1 in range(1, questions + 1):
+
+        # randomly generates numbers between user input
+        equation_num1 = random.randint(lowest, highest)
+        equation_num2 = random.randint(lowest, highest)
+
+        # randomly chooses operation
+        operation = random.choice(operations)
+
+        # generates equations and equation answers from randomly generated operations and numbers
+        display_equation = "What does {} {} {} equal? ".format(equation_num1, operation, equation_num2)
+        equation = "{} {} {}".format(equation_num1, operation, equation_num2)
+        correct_answer = eval(equation)
         print()
-        statement_generator(feedback, "*")
-        print()
-        right += 1
-        result = "Correct"
-    else:
-        feedback = "~ Sorry, your answer was  incorrect. The answer was {}. ~".format(correct_answer)
-        print()
-        statement_generator(feedback, "~")
-        print()
-        wrong += 1
-        result = "Incorrect"
 
-    # takes round number, equation, user answer, and result to be printed out in quiz history
-    equation_answer = "Equation {}: {} =?   Your answer: {}  |  {}".format(item, equation, answer, result)
-    quiz_history.append(equation_answer)
+        # generates answer for any equation randomly generated
+        answer = int_check(display_equation, -(highest * highest), (highest * highest))
 
-# calculates quiz stats
-percent_r = right / questions * 100
-percent_w = wrong / questions * 100
+        # compares user answer to actual answer and displays feedback
+        if answer == correct_answer:
+            feedback = "* Your answer of {} was correct. Great job! *".format(answer)
+            print()
+            statement_generator(feedback, "*")
+            print()
+            right += 1
+            result = "Correct"
+        else:
+            feedback = "~ Sorry, your answer was  incorrect. The answer was {}. ~".format(correct_answer)
+            print()
+            statement_generator(feedback, "~")
+            print()
+            wrong += 1
+            result = "Incorrect"
 
-# prints game history
-print()
-print("| | | | | Quiz History | | | | |")
-print()
-for item in quiz_history:
-    print(item)
+        # takes round number, equation, user answer, and result and adds them to list
+        equation_answer = "Equation {}: {} =?   Your answer: {}  |  {}".format(item1, equation, answer, result)
+        quiz_history.append(equation_answer)
 
-# prints game statistics
-print()
-print("| | | | | Quiz Stats | | | | |")
-print()
-print("Answers Correct: {} | {:.0f}%".format(right, percent_r))
-print("Answers Incorrect: {} | {:.0f}%".format(wrong, percent_w))
-print()
+    # calculates quiz stats
+    percent_r = right / questions * 100
+    percent_w = wrong / questions * 100
 
-# if user wants to do quiz again user can redo it without having to re run the code
-go_again = string_checker("Would you like to do the quiz again? ", "")
+    # prints game history
+    print()
+    print("| | | | | Quiz History | | | | |")
+    print()
+    for item in quiz_history:
+        print(item)
 
-if go_again == "":
+    # prints game statistics
+    print()
+    print("| | | | | Quiz Stats | | | | |")
+    print()
+    print("Answers Correct: {} | {:.0f}%".format(right, percent_r))
+    print("Answers Incorrect: {} | {:.0f}%".format(wrong, percent_w))
+    print()
 
+    # if user presses enter key program loops back to start of quiz
+    go_again = input("Press <enter> to go again or any other key to quit ")
+else:
+    # if user presses any other key quiz ends
+    print("Thank you for doing the quiz :)")
